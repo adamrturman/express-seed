653837c529d7d86c68218937dcf57a61a5c633d7
adamrturman <adamrturman@gmail.com>
2020-12-07 10:49:04 -0600

    inital commit, new file for server and client ports

---
+++ config/ports.js
@@ -0,0 +1,9 @@
+// define server and client ports
+// used for cors and local port declaration
+const serverDevPort = 4741
+const clientDevPort = 7165
+
+module.exports = {
+    serverDevPort,
+    clientDevPort
+}
