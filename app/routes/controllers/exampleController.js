deba570074b89b7dbf42fea95ae83104f95f160e
adamrturman <adamrturman@gmail.com>
2020-12-17 12:27:40 -0600

    example patch refactored to controller

--- app/routes/controllers/exampleController.js
+++ app/routes/controllers/exampleController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 const handle404 = require('../../../lib/custom_errors')
+const { requireOwnership } = require('../../../lib/custom_errors')
 const passport = require('passport')
 const requireToken = passport.authenticate('bearer', { session: false })
 const Example = require('../../models/example')
@@ -44,8 +45,30 @@ const post = (req, res, next) => {
       .catch(next)
   }
 
+  const patch = (req, res, next) => {
+    // if the client attempts to change the `owner` property by including a new
+    // owner, prevent that by deleting that key/value pair
+    delete req.body.example.owner
+  
+    Example.findById(req.params.id)
+      .then(handle404)
+      .then(example => {
+        // pass the `req` object and the Mongoose record to `requireOwnership`
+        // it will throw an error if the current user isn't the owner
+        requireOwnership(req, example)
+  
+        // pass the result of Mongoose's `.update` to the next `.then`
+        return example.updateOne(req.body.example)
+      })
+      // if that succeeded, return 204 and no JSON
+      .then(() => res.sendStatus(204))
+      // if an error occurs, pass it to the handler
+      .catch(next)
+  }
+
   module.exports = {
       get,
       show,
-      post
+      post,
+      patch
   }
