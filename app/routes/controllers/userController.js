79b7cc4845a1da76ae07f9c35c233887ee06f825
adamrturman <adamrturman@gmail.com>
2020-12-17 10:19:21 -0600

    refactored signin to usercontroller

--- app/routes/controllers/userController.js
+++ app/routes/controllers/userController.js
@@ -39,6 +39,45 @@ const signUp = (req, res, next) => {
       .catch(next)
   }
 
+const signIn = (req, res, next) => {
+  const pw = req.body.credentials.password
+  let user
+
+  // find a user based on the email that was passed
+  User.findOne({ email: req.body.credentials.email })
+    .then(record => {
+      // if we didn't find a user with that email, send 401
+      if (!record) {
+        throw new BadCredentialsError()
+      }
+      // save the found user outside the promise chain
+      user = record
+      // `bcrypt.compare` will return true if the result of hashing `pw`
+      // is exactly equal to the hashed password stored in the DB
+      return bcrypt.compare(pw, user.hashedPassword)
+    })
+    .then(correctPassword => {
+      // if the passwords matched
+      if (correctPassword) {
+        // the token will be a 16 byte random hex string
+        const token = crypto.randomBytes(16).toString('hex')
+        user.token = token
+        // save the token to the DB as a property on user
+        return user.save()
+      } else {
+        // throw an error to trigger the error handler and end the promise chain
+        // this will send back 401 and a message about sending wrong parameters
+        throw new BadCredentialsError()
+      }
+    })
+    .then(user => {
+      // return status 201, the email, and the new token
+      res.status(201).json({ user: user.toObject() })
+    })
+    .catch(next)
+} 
+
   module.exports = {
-      signUp
+      signUp,
+      signIn
   }
